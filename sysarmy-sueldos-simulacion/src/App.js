043dd7fc1ad974f1f095045d1fbea2510a2f995d
Sebastian Waisbrot <seppo0010@gmail.com>
2018-07-26 13:29:55 -0300

    Results preview

--- sysarmy-sueldos-simulacion/src/App.js
+++ sysarmy-sueldos-simulacion/src/App.js
@@ -37,6 +37,38 @@ const benefits = [
     'Bonos por desempeño',
 ]
 
+class Result extends Component {
+  render() {
+    return (
+      <div className="result">
+        <div><label>Me identifico: </label><strong>{this.props.data['Me identifico']}</strong></div>
+        <div><label>Tengo: </label><strong>{this.props.data['Tengo']}</strong></div>
+        <div><label>Años de experiencia: </label><strong>{this.props.data['Años de experiencia']}</strong></div>
+        <div><label>Años en el puesto actual: </label><strong>{this.props.data['Años en el puesto actual']}</strong></div>
+        <div><label>¿Cuánta gente a cargo?: </label><strong>{this.props.data['¿Cuánta?']}</strong></div>
+        <div><label>Nivel de estudios alcanzado: </label><strong>{this.props.data['Nivel de estudios alcanzado']}</strong></div>
+        <div><label>Estado: </label><strong>{this.props.data['Estado']}</strong></div>
+        <div><label>¿Estudiaste una carrera relacionada a Informática?: </label><strong>{{
+          'Ingeniería informática': 'Sí',
+          '': 'No',
+        }[this.props.data['Carrera']]}</strong></div>
+        <div><label>Trabajo de: </label><strong>{this.props.data['Trabajo de']}</strong></div>
+        <div><label>Tecnologías que utilizás: </label><strong>{
+          this.props.data['Tecnologías que utilizás'].concat(
+            this.props.data['Tecnologías que utilizás.1'],
+            this.props.data['Tecnologías que utilizás.2'],
+            this.props.data['Tecnologías que utilizás.3'],
+            this.props.data['Tecnologías que utilizás.4']
+          ).join(', ')
+        }</strong></div>
+        <div><label>Cantidad de empleados en la empresa: </label><strong>{this.props.data['Cantidad de empleados']}</strong></div>
+        <div><label>Beneficios Extra: </label><strong>{this.props.data['Beneficios Extra'].join(', ')}</strong></div>
+        <div><label>Sueldo estimado: </label><strong>{this.props.salary}</strong></div>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   state = {
     'Me identifico': '',
@@ -54,6 +86,7 @@ class App extends Component {
     'Tecnologías que utilizás.4': [],
     'Cantidad de empleados': '',
     'Beneficios Extra': [],
+    'results': []
   };
 
   handleChange = event => {
@@ -178,8 +211,10 @@ class App extends Component {
               InputLabelProps={{
                 shrink: true,
               }}
+              inputProps={{
+                min: 0,
+              }}
               margin="normal"
-              min={0}
             />
           </FormControl>
         </div>
@@ -325,6 +360,11 @@ class App extends Component {
           <Button variant="outlined" color="primary">
             Calcular sueldo
           </Button>
+        </div>
+        <div className="result-list">
+          <div style={{width: '440px'}}>
+            <Result data={this.state} salary="" />
+          </div>
         </div>
       </div>
     );
